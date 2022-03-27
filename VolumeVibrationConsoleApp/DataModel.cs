29af16925b9ce2f563712289e31b2eab3a6bf5ad
Ahmet Faruk Alaçam <killtheswots@gmail.com>
2022-03-28 02:21:37 +0300

    Add project files.

---
+++ VolumeVibrationConsoleApp/DataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeVibrationConsoleApp
+{
+    public class DataModel
+    {
+        public int NO { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double SES { get; set; }
+    }
+}
---
+++ VolumeVibrationConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolumeVibrationConsoleApp
+{
+    internal class Program
+    {
+        private static Stopwatch stopwatch;
+
+        private static void Main(string[] args)
+
+        {
+            bool IsPedalPushed = false;
+            stopwatch = new Stopwatch();
+            var a = File.ReadAllText(@"C:\WorkStation\Garbage\Volume-VibrationConsoleApp\VolumeVibrationConsoleApp\VolumeVibrationConsoleApp\Data.json");
+            var b = JsonConvert.DeserializeObject<List<DataModel>>(a);
+            Console.WriteLine($"Başla");
+
+            List<DataModel> list = new List<DataModel>();
+            foreach (DataModel dataModel in b)
+            {
+                Task.Delay(10).Wait();
+
+                if (list.Count < 30)
+                {
+                    list.Add(dataModel);
+                }
+                else
+                {
+                    list.RemoveAt(0);
+
+                    if (dataModel.SES > 8000)
+                    {
+                        if (list.Max(x => x.X) - list.Min(x => x.X) >= 0.12)
+                        {
+                            if (!IsPedalPushed)
+                            {
+                                stopwatch.Start();
+                                IsPedalPushed = true;
+                                Console.WriteLine($"Başlangıç Adım: {dataModel.NO}");
+                            }
+                        }
+                        else
+                        {
+                            if (IsPedalPushed)
+                            {
+                                stopwatch.Stop();
+                                IsPedalPushed = false;
+                                TimeSpan ts = stopwatch.Elapsed;
+                                stopwatch.Restart();
+
+                                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                ts.Hours, ts.Minutes, ts.Seconds,
+                                ts.Milliseconds / 10);
+                                Console.WriteLine("RunTime " + elapsedTime);
+                                Console.WriteLine($"Bitiş Adım: {dataModel.NO}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (IsPedalPushed)
+                        {
+                            stopwatch.Stop();
+                            IsPedalPushed = false;
+                            TimeSpan ts = stopwatch.Elapsed;
+                            stopwatch.Restart();
+
+                            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                            ts.Hours, ts.Minutes, ts.Seconds,
+                            ts.Milliseconds / 10);
+                            Console.WriteLine("RunTime " + elapsedTime);
+                            Console.WriteLine($"Bitiş Adım: {dataModel.NO}");
+                        }
+                    }
+
+                    list.Add(dataModel);
+                }
+            }
+        }
+    }
+}
